a121010b08b15679eaed6975cf722c71d3be05bb
jmukamani <j.mukamani@alustudent.com>
2023-02-25 02:14:01 -0800

    Dynamic script

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    argstr = 'argument'
+    lengthofargs = len(args)
+
+    if lengthofargs == 0:
+        argstr = argstr + 's.'
+    elif lengthofargs == 1:
+         argstr = argstr + ':'
+     else:
+         argstr = argstr + 's:'
+
+         print(lengthofargs, argstr)
+
+         if lengthofargs > 0:
+             for i in range(lengthofargs):
+                 print('{}: {}'.format(i+1, args[i]))
