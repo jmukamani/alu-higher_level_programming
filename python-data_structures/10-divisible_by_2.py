411215bf6281a28f8cc219bf1ff9535d1afa7bc2
jmukamani <j.mukamani@alustudent.com>
2023-03-03 05:05:06 -0800

    Switch

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     multiple = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            multiples.append(True)
+            multiple.append(True)
         else:
-            multiples.append(False)
-    return(multiples)
+            multiple.append(False)
+    return(multiple)
