f870e0770c2949f0eb42a0c9350568c2745215a4
jmukamani <j.mukamani@alustudent.com>
2023-03-07 03:39:00 -0800

    Set and Dictionary

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -11,7 +11,7 @@ def roman_to_int(roman_string):
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
-    if not isinstance(roma_string, str):
+    if not isinstance(roman_string, str):
         return 0
 
     rom_no = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
