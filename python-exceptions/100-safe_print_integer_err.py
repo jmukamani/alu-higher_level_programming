3c7038712e5f160b432d923829fe74e5accecad2
jmukamani <j.mukamani@alustudent.com>
2023-03-08 01:10:10 -0800

    Exception

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -2,8 +2,8 @@
 def safe_print_integer_err(value):
     import sys
     try:
-        print("{:d}".formaat(value))
-        return True
+        print("{:d}".format(value))
+        return (True)
     except Exception as i:
         sys.stderr.write("Exception: {}\n".format(i))
-        return False
+        return (False)
