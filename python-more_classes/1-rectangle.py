d53953481d78662c5f85892efa2317dd8bf17191
jmukamani <j.mukamani@alustudent.com>
2023-03-16 03:55:03 -0700

    defination of a rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -4,18 +4,21 @@
 
 class Rectangle:
     """This module defines a class attribute Rectangle."""
+    
     def __init__(self, width=0, height=0):
+        """Initialize an instance of the Rectangle with width and height.
+        """
         self.width = width
         self.height = height
 
     @property
-    """Getter method for the width."""
     def width(self):
+        """Getter method for the attribute __width."""
         return self.__width
 
     @width.setter
-    """Get/set width of the rectangle."""
     def width(self, value):
+        """Setter method for the __with attribute."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -23,13 +26,13 @@ class Rectangle:
         self.__width = value
 
     @property
-    """Getter method for the height."""
     def height(self):
+        """Getter method for the attribute __height."""
         return self.__height
 
     @height.setter
-    """Get/set height of the rectangle."""
     def height(self, value):
+        """Setter method for the __height attributr."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
