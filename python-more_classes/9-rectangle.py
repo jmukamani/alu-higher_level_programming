4c7182902ed2c62b081b9f42903001930384eb18
jmukamani <j.mukamani@alustudent.com>
2023-03-17 04:16:50 -0700

    Rectanglee

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
-"""This module represents a rectangle."""
+"""This module represents a square."""
 
 
 class Rectangle:
-    """This defines a rectangle."""
+    """Defines a rectangle."""
+
     number_of_instances = 0
-    """public class atrribute initialized to 0."""
-    print_symbol = '#'
-    """used as symbol for string representation."""
+    print_symbol = "#"
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
+
+   @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
 
     def __init__(self, width=0, height=0):
         self.width = width
@@ -58,7 +72,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         return symbol + "\n".join(str(self.print_symbol) * self.width
-                                for i in range(self.height))
+                for i in range(self.height))
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.width, self.height)
