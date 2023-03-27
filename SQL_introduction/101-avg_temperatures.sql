d2e33bb40f0140938278708ef2260a341440f779
jmukamani <j.mukamani@alustudent.com>
2023-03-27 06:55:01 -0700

    Advanced

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Import in hbtn_0c_0 database
-SELECT `city`, AVG(`value`) 'avg_temp' FROM temeperatures GROUP BY `city` ORDER BY `avg_temp` DESC;
+SELECT `city`, AVG(`value`) 'avg_temp' FROM temperatures GROUP BY `city` ORDER BY `avg_temp` DESC;
