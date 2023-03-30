5b8215d003ef16040a3ce66f10599d1957793f73
jmukamani <j.mukamani@alustudent.com>
2023-03-30 03:37:46 -0700

    Sql queries

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Lists all cities of California than can be found in hbtn_0d_usa
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE nam = "California") ORDER BY id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id;
