6ed37c148e8b39049b13b945b492ac1c6b524f8b
jmukamani <j.mukamani@alustudent.com>
2023-03-30 03:47:00 -0700

    Sql queries

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- Creates table 'id_not_null on MYSQL server
-CREATE TABLE IF NOT EXISTS id_not_null(id NULL INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256));
