6831b709b15ae60d9db464317edd01feb65955d3
jmukamani <j.mukamani>
2023-04-23 00:38:09 -0700

    ORMs

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `status`")
-    [print(state) for state in c.fetcha11()]
+    [print(state) for state in c.fetchall()]
