7b66c14736645ff9d55dff0f81f5d58d896f2ed6
jmukamani <j.mukamani@alustudent.com>
2023-06-12 06:23:40 -0700

    warmup

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const number = Number(process.argv[2]);
-if (isNaN(Number)) {
+if (isNaN(number)) {
   console.log('Not a number');
 } else {
   console.log(`My number: ${number}`);
