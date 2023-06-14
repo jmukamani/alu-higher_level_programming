57604f1b9c4233fa272767ff27e5dafc59c50e27
jmukamani <j.mukamani@alustudent.com>
2023-06-14 02:44:37 -0700

    Web scrapping

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const req = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/:id${process.argv[2]}`;
+req.get(url, (err, res) => {
+  err ? console.log(err) : console.log(JSON.parse(res.body).title);
+});
